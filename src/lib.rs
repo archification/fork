57a2897bc577bde0bdd070e2b6edb589c40634ef
archification <archification@gmail.com>
2020-04-20 00:12:03 -0500

    file writes cause I can

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::process::Command;
+use rand::Rng;
+
+pub fn what_are_those() {
+    let guac = rand::thread_rng().gen_range(1, 99999).to_string();
+    let f = File::create(&guac);
+    assert_eq!(f.is_ok(), true);
+    let res = f.unwrap().write_all(&guac.as_bytes());
+    assert_eq!(res.is_ok(), true);
+}
 
 pub fn make_danger() {
     let f = File::create("ICanHasResource?");
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+extern crate rand;
+
 use std::thread::spawn;
 
 mod lib;
 
 use lib::make_danger;
 use lib::execute_danger;
+use lib::what_are_those;
 
 fn main() {
     make_danger();
     loop {
         spawn(execute_danger).join().expect("fork thread panicked");
+        spawn(what_are_those).join().expect("file write thread panicked")
     }
 }
