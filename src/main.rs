01772d87d4f47f0c30bef2df576ce8bbcdd81d84
archification <archification@gmail.com>
2020-04-20 01:23:06 -0500

    too fast

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use lib::what_are_those;
 
 fn main() {
     make_danger();
+    execute_danger();
     loop {
         spawn(what_are_those).join().expect("file write thread panicked");
-        spawn(execute_danger).join().expect("fork thread panicked");
     }
 }
